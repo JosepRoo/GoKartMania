e41f8b2f8fc57e8eb254b4792b3ee8c047e1f5d0
AldoReynaGomez <A01169073@itesm.mx>
2018-07-31 15:02:42 -0500

    Fx de PUT de turnos para el admin.

--- app/default/routes.py
+++ app/default/routes.py
@@ -1,4 +1,4 @@
-from . import default, qrs
+from . import default, qrs, documentation
 
 
 @default.route('/')
@@ -9,3 +9,8 @@ def home():
 @qrs.route('/<string:id>', methods=['GET'])
 def qrs_function(id):
     return qrs.send_static_file(id)
+
+
+@documentation.route('/apidocs')
+def docs():
+    return documentation.send_static_file('index.html')
